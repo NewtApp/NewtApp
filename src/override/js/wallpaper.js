9ea643e9edd02ac47956f00c8428920f4f8152b4
sir-lagsalot <gabephua@gmail.com>
2017-02-14 22:27:48 +1100

    Got JSON working

--- src/override/js/wallpaper.js
+++ src/override/js/wallpaper.js
@@ -1,3 +1,5 @@
-var response = jQuery.getJSON("https://pixabay.com/api/?key=2303081-d69651e05413a133d621bfd4b&q=landscape&image_type=photo&pretty=true");
-console.log(response);
-console.log(response.readyState);
+var wpresponse = jQuery.getJSON("https://pixabay.com/api/?key=2303081-d69651e05413a133d621bfd4b&q=landscape&image_type=photo&pretty=true", function(data){
+  var wpcropped = wpresponse.responseText
+  console.log(wpcropped)
+  var wparray = wpcropped.hits[1]
+});
