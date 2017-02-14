ef1d2fd612ed57675ec584d6086829f1916fbd8f
Gabriel Phua <Whyte@Gabriels-Macbook-Pro.local>
2017-02-14 18:47:50 +1100

    next!

---
+++ old/README.md
@@ -0,0 +1,8 @@
+# NewtApp
+A modular, widget based new tab extension for Chromium.
+
+Beta settings -
+
+Extension has no restriction on viewed websites, for debugging convenience
+
+JS files are not minified
---
+++ src/override/js/wallpaper.js
@@ -0,0 +1,3 @@
+var response = jQuery.getJSON("https://pixabay.com/api/?key=2303081-d69651e05413a133d621bfd4b&q=landscape&image_type=photo&pretty=true");
+console.log(response);
+console.log(response.readyState);
