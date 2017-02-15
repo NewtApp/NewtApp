33ef5c03d53e211616ae24c40f5752015edd627b
WhyteBear <gabrielphua@gmail.com>
2017-02-16 08:13:18 +1100

    Added fadein

---
+++ js/effects.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("html").hide().fadeIn(1000);});
--- js/test.js
+++
@@ -1,4 +0,0 @@
-
-  var width = window.innerwidth
-  var height = window.innerheight
-  alert("The viewport you're looking at right now is " + width + "x" + height + "px.")
