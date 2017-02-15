b5ae2c8c8c55a9bae8ecfef821f60607516503ea
sir-lagsalot <gabephua@gmail.com>
2017-02-15 16:15:57 +1100

    should've fixed this earlier.  	`

--- src/override/js/wallpaper.js
+++
@@ -1,5 +0,0 @@
-var wpresponse = jQuery.getJSON("https://pixabay.com/api/?key=2303081-d69651e05413a133d621bfd4b&q=landscape&image_type=photo&pretty=true", function(data){
-  var wpcropped = wpresponse.responseText
-  console.log(wpcropped)
-  var wparray = wpcropped.hits[1]
-});
