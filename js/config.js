9f9e43a511d868284f01d48d362c36d33d07e4b8
sir-lagsalot <gabephua@gmail.com>
2017-02-15 23:24:47 +1100

    Minified for test production

--- js/config.js
+++
@@ -1,6 +0,0 @@
-config = {}
-
-config.settings = {
-  "key" : "2303081-d69651e05413a133d621bfd4b",
-  "url:" : "https://pixabay.com/api/?key=2303081-d69651e05413a133d621bfd4b&q=landscape&image_type=photo&pretty=true",
-}
